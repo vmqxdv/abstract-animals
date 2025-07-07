dd501f80f756aa70d18f84b8910f6c2bd2fdee98
vmqxdv <vquintilio@gmail.com>
2025-07-07 17:51:02 +0200

    feat: Add Fly and Swim interfaces

---
+++ src/org/lessons/java/animals/Fly.java
@@ -0,0 +1,5 @@
+package org.lessons.java.animals;
+
+public interface Fly {
+  public void fly();
+}
---
+++ src/org/lessons/java/animals/Swim.java
@@ -0,0 +1,5 @@
+package org.lessons.java.animals;
+
+public interface Swim {
+  public void swim();
+}
