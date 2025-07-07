27209da1517cbe0e4caf4b2a89275d104b50ee8d
vmqxdv <vquintilio@gmail.com>
2025-07-07 17:21:41 +0200

    feat: Implement Dog class

---
+++ src/org/lessons/java/animals/Dog.java
@@ -0,0 +1,37 @@
+package org.lessons.java.animals;
+
+public class Dog extends Animal {
+
+  private String breed;
+  private boolean isTrained;
+
+  // -- Breed
+  public String getBreed() {
+    return this.breed;
+  }
+
+  public void setBreed(String breed) {
+    this.breed = breed;
+  }
+
+  // -- Is Trained?
+  public boolean isTrained() {
+    return this.isTrained;
+  }
+
+  public void isTrained(boolean isTrained) {
+    this.isTrained = isTrained;
+  }
+
+  // -- Inherited
+  @Override
+  public void makeSound() {
+    System.out.println("Woof woof!!");
+  }
+
+  @Override
+  public void eat() {
+    System.out.println("*Mangia carne*");
+  }
+
+}
