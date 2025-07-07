8426a85ae1839dc180583528ca9861815771f215
vmqxdv <vquintilio@gmail.com>
2025-07-07 17:09:04 +0200

    first commit

---
+++ src/org/lessons/java/animals/Animal.java
@@ -0,0 +1,56 @@
+package org.lessons.java.animals;
+
+public abstract class Animal {
+
+  private String name;
+  private int age;
+  private int sex;
+  private float weight;
+  private float height;
+
+  // -- Name
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  // -- Age
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  // -- Sex
+  public int getSex() {
+    return this.sex;
+  }
+
+  public void setSex(int sex) {
+    this.sex = sex;
+  }
+
+  // -- Weight
+  public float getWeight() {
+    return this.weight;
+  }
+
+  public void setWeight(float weight) {
+    this.weight = weight;
+  }
+
+  // -- Height
+  public float getHeight() {
+    return this.height;
+  }
+
+  public void setHeight(float height) {
+    this.height = height;
+  }
+
+}
