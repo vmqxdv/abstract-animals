79a80b1bf7ec3b5ac2bfc795fc5a5c975004d6a4
vmqxdv <vquintilio@gmail.com>
2025-07-07 17:41:36 +0200

    feat: Added Dolphin class constructors

--- src/org/lessons/java/animals/Dolphin.java
+++ src/org/lessons/java/animals/Dolphin.java
@@ -5,6 +5,18 @@ public class Dolphin extends Animal {
   private double swimSpeed;
   private int maxDiveDepth;
 
+  public Dolphin() {
+    super();
+    this.swimSpeed = 0;
+    this.maxDiveDepth = 0;
+  }
+
+  public Dolphin(String name, int age, int sex, float weight, float height, double swimSpeed, int maxDiveDepth) {
+    super(name, age, sex, weight, height);
+    this.swimSpeed = swimSpeed;
+    this.maxDiveDepth = maxDiveDepth;
+  }
+
   // -- Swim Speed
   public double getSwimSpeed() {
     return this.swimSpeed;
